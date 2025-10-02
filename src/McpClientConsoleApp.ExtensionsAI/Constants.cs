81e9317e00f3c39659c1c2584ab569b7072c1d15
Marco Minerva <marco.minerva@gmail.com>
2025-10-02 18:53:40 +0200

    Delete src/McpClientConsoleApp.ExtensionsAI/Constants.cs

--- src/McpClientConsoleApp.ExtensionsAI/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace McpClientConsoleApp.ExtensionsAI;
-
-public static class Constants
-{
-    public const string Endpoint = "https://commonservice-eastus2.openai.azure.com/";
-    public const string DeploymentName = "gpt-4.1";
-    public const string ApiKey = "09a009470b994fbbaa905b8af4584cff";
-}
