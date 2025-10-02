a9115ca3746e3c09fd79c93b89c8f557998b96f2
Marco Minerva <marco.minerva@gmail.com>
2025-10-02 18:54:37 +0200

    Delete src/McpClientConsoleApp/Constants.cs

--- src/McpClientConsoleApp/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace McpClientConsoleApp;
-
-public static class Constants
-{
-    public const string Endpoint = "";
-    public const string DeploymentName = "";
-    public const string ApiKey = "";
-}
